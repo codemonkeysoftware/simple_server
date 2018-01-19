2175bd4186bc3ed3433e9a535aae9b8a2cf110f2
Landon Dyck <landon@dyck.us>
2018-01-18 17:14:08 -0700

    init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultPort = ":5000"
+
+func main() {
+	portFlag := flag.String("p", "", "port number")
+	flag.Parse()
+	var port string
+	if *portFlag == "" {
+		log.Println("no port specified, using default port ", defaultPort)
+		port = defaultPort
+	} else {
+		port = *portFlag
+		if !strings.HasPrefix(port, ":") {
+			port = ":" + port
+		}
+	}
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
